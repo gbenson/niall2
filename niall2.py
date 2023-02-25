e66d4253db9186cda7437826e6eb11751dbcaab7
Gary Benson <gary@gbenson.net>
2023-02-25 21:00:00 +0000

    fafing

--- niall2.py
+++ niall2.py
@@ -4,12 +4,25 @@ class Robot:
     def __init__(self, words):
         self.words = words
 
+    def prompt(self, color, who):
+        return f"\x1b[{color}m{who}>\x1B[0m "
+
+    def niall_prompt(self):
+        return self.prompt(33, "Niall")
+
     def run(self):
-        pass
+        print(self.niall_prompt(),
+              "Hi, I'm Niall, how may I help you?")
+        while True:
+            s = input(self.prompt(35, "User"))
+            print("You typed", s)
 
 def main(*args):
     with shelve.open("words") as state:
-        Robot(state).run()
+        try:
+            Robot(state).run()
+        except EOFError:
+            print()
 
 if __name__ == "__main__":
     main()
