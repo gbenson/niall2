7f8f520237eda3d026cc757c8afda135d441c3b7
Gary Benson <gary@gbenson.net>
2023-03-06 12:30:54 +0000

    All the webclient except the API calls

--- niall2.js
+++ niall2.js
@@ -1,23 +1,30 @@
-const inputValue = document.getElementById("input");
-const messagesContainer = document.getElementById("messages");
+const theInput = document.getElementById("input");
+const messages = document.getElementById("messages");
 
 document.addEventListener("DOMContentLoaded", () => {
-  inputValue.addEventListener("keydown", (e) => {
+  theInput.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
-      let input = inputValue.value;
-      inputValue.value = "";
-      process(input);
+      let userInput = theInput.value;
+      theInput.value = "";
+      processInput(userInput);
     }
   });
 });
 
-function process(input)
-{
-  input = input.trim();
+function processInput(userInput) {
+  userInput = userInput.trim();
+  addToChat("user", userInput);
 
-  const div = document.createElement("div")
-  div.classList.add("message")
-  div.classList.add("from-user")
-  div.innerText = input;
-  messagesContainer.appendChild(div);
+  const niallDiv = addToChat("niall", "...");
+  niallDiv.innerText = "hello from niall"
+}
+
+function addToChat(sender, message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.classList.add(`from-${sender}`);
+  div.innerText = message;
+  messages.appendChild(div);
+  messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  return div;
 }
