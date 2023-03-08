10df4a7f5481172a27331b4ff4e3db29e67685e9
Gary Benson <gary@gbenson.net>
2023-03-08 00:09:54 +0000

    API calls :)

--- niall2.js
+++ niall2.js
@@ -11,12 +11,31 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function processInput(userInput) {
+const apiEndpointURL = ("https://nrtt8bz8be.execute-api.us" +
+                        "-east-1.amazonaws.com/prod/niall2");
+
+async function processInput(userInput) {
   userInput = userInput.trim();
   addToChat("user", userInput);
 
   const niallDiv = addToChat("niall", "...");
-  niallDiv.innerText = "hello from niall"
+
+  const response = await fetch(apiEndpointURL, {
+    method: "POST",
+    headers: {
+      // "application/json" would be better here, but
+      // then CORS preflights every single request :(
+      "Content-Type": "text/plain",
+    },
+    body: JSON.stringify({
+      user_input: userInput,
+      dry_run: true,
+    }),
+  });
+  const json = await response.json();
+  console.log(json);
+
+  niallDiv.innerText = json["niall_output"];
 }
 
 function addToChat(sender, message) {
